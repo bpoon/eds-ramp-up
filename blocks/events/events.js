549a66a9de7f31cc3aacbc066f361319890b089c
Bernard Poon <bybpoon@gmail.com>
2024-03-25 14:39:17 -0700

    Fix errors

--- blocks/events/events.js
+++ blocks/events/events.js
@@ -1,20 +1,17 @@
-
-
-
 export default async function decorate(block) {
-    const eventslist = document.createElement('div');
-    eventslist.className = 'events-list';
-    eventslist.setAttribute('role', 'eventlist');
-    const events = [...block.children];
-    events.forEach((event, i) => {
-        const title = event.firstElementChild;
-        const type = event.lastElementChild.innerHTML;
-        const eventdiv = document.createElement('div');
-        eventdiv.className = `event-type-${type}`;
-        eventdiv.setAttribute('role', 'tabpanel');
-        eventdiv.innerHTML = title.innerHTML;
-        eventslist.append(eventdiv);
-    });
-    block.textContent = '';
-    block.append(eventslist);
+  const eventslist = document.createElement('div');
+  eventslist.className = 'events-list';
+  eventslist.setAttribute('role', 'eventlist');
+  const events = [...block.children];
+  events.forEach((event) => {
+    const title = event.firstElementChild;
+    const type = event.lastElementChild.innerHTML;
+    const eventdiv = document.createElement('div');
+    eventdiv.className = `event-type-${type}`;
+    eventdiv.setAttribute('role', 'tabpanel');
+    eventdiv.innerHTML = title.innerHTML;
+    eventslist.append(eventdiv);
+  });
+  block.textContent = '';
+  block.append(eventslist);
 }
