874358bbfc524da12035749ceb18394e443a4c63
Bernard Poon <bybpoon@gmail.com>
2024-03-26 17:26:42 -0700

    test

--- blocks/hello/hello.js
+++ blocks/hello/hello.js
@@ -1,7 +1,7 @@
 export default async function decorate(block) {
   const hello = document.createElement('div');
   hello.className = 'hello';
-  hello.innerHTML = block.firstElementChild.innerHTML;
+  hello.innerHTML = block.firstElementChild.innerText;
   block.textContent = '';
   block.append(hello);
 }
